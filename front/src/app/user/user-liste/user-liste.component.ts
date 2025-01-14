75d9a9022957d8918bc996339a60396ccb553f0a
rihaya493 <rihaya493@gmail.com>
2025-01-14 15:27:51 +0100

    user module

---
+++ front/src/app/user/user-liste/user-liste.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-liste',
+  imports: [],
+  templateUrl: './user-liste.component.html',
+  styleUrl: './user-liste.component.css'
+})
+export class UserListeComponent {
+
+}
